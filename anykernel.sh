3d8e9101454d4d47b37c515397922524406e8f68
SkyFaklist <45099567+SkyFaklist@users.noreply.github.com>
2023-01-17 23:20:55 +0800

    add FPS change

--- anykernel.sh
+++ anykernel.sh
@@ -36,5 +36,45 @@ ramdisk_compression=auto;
 # boot install
 dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
 
+# Marsia Custom sh
+case "$ZIPFILE" in
+  *66fps*|*66hz*)
+    ui_print "  • Setting 66 Hz refresh rate"
+    patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=1"
+    ;;
+  *69fps*|*69hz*)
+    ui_print "  • Setting 69 Hz refresh rate"
+    patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=2"
+    ;;
+  *72fps*|*72hz*)
+    ui_print "  • Setting 72 Hz refresh rate"
+    patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=3"
+    ;;
+  *75fps*|*75hz*)
+    ui_print "  • Setting 75 Hz refresh rate"
+    patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=4"
+    ;;
+  *81fps*|*81hz*)
+    ui_print "  • Setting 81 Hz refresh rate"
+    patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=5"
+    ;;
+  *84fps*|*84hz*)
+    ui_print "  • Setting 84 Hz refresh rate"
+    patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=6"
+    ;;
+  *90fps*|*90hz*)
+    ui_print "  • Setting 90 Hz refresh rate"
+    patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=7"
+    ;;
+  *)
+    patch_cmdline "msm_drm.framerate_override" ""
+    fr=$(cat /sdcard/framerate_override | tr -cd "[0-9]");
+    [ $fr -eq 66 ] && ui_print "  • Setting 66 Hz refresh rate" && patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=1"
+    [ $fr -eq 69 ] && ui_print "  • Setting 69 Hz refresh rate" && patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=2"
+    [ $fr -eq 72 ] && ui_print "  • Setting 72 Hz refresh rate" && patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=3"
+    [ $fr -eq 75 ] && ui_print "  • Setting 75 Hz refresh rate" && patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=4"
+    ;;
+esac
+
 write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
 ## end boot install
